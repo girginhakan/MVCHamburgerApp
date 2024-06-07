bff65ae29da66eb070b3cfa1b4e4be406569ab55
Hakan Girgin <hakangirgin7@gmail.com>
2024-06-08 00:06:21 +0300

    Entitiyler, Database ve Admin için Area oluşturuldu. Gerekli ayarlar yapıldı.

---
+++ MVCHamburgerApp/Data/HamburgerDbContext.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVCHamburgerApp.Data.Entities;
+using System.Collections.Generic;
+
+namespace MVCHamburgerApp.Data
+{
+    public class HamburgerDbContext:IdentityDbContext<AppUser,IdentityRole<int>,int>
+    {
+        public HamburgerDbContext(DbContextOptions<HamburgerDbContext> options):base(options) { }
+
+        public DbSet<ExtraTopping> ExtraToppings { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var hasher = new PasswordHasher<AppUser>();
+            builder.Entity<AppUser>()
+                .HasData(new AppUser
+                {
+                    Id = 1,
+                    Name = "adminName",
+                    Surname = "adminSurname",
+                    UserName = "admin",
+                    NormalizedUserName = "ADMIN",
+                    Email = "admin@mail.com",
+                    NormalizedEmail = "ADMIN@MAIL.COM",
+                  
+                    EmailConfirmed = true,
+                    PhoneNumberConfirmed = true,
+                    PhoneNumber = "-",
+                    PasswordHash = hasher.HashPassword(null, "Az*123456"),
+                    SecurityStamp = Guid.NewGuid().ToString()
+                });
+
+
+            builder.Entity<IdentityRole<int>>()
+                .HasData(new IdentityRole<int>
+                {
+                    Id = 1,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                });
+
+
+            builder.Entity<IdentityRole<int>>()
+                .HasData(new IdentityRole<int>
+                {
+                    Id= 2,
+                    Name="Musteri",
+                    NormalizedName="MUSTERI"
+                });
+
+            builder.Entity<IdentityUserRole<int>>()
+                .HasData(new IdentityUserRole<int>
+                {
+                    UserId = 1,
+                    RoleId = 1
+                });
+        }
+    }
+}
---
+++ MVCHamburgerApp/Migrations/20240607124033_Menülere resim eklemek için Menu tablosuna gereken proplar eklendi.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCHamburgerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class MenülereresimeklemekiçinMenutablosunagerekenproplareklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PictureFile",
+                table: "Menus",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureName",
+                table: "Menus",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "76e254ed-f5bb-48b6-a8cc-3bb8a0833834", "AQAAAAIAAYagAAAAECuDXnGROWRvRboq18aCIEPZMfqJ0P84RY8+VmTK5HSAP8PjkEUUPqdhqVdxjoLyKg==", "fb3eec46-fa74-4ed2-bc66-961c45895997" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureFile",
+                table: "Menus");
+
+            migrationBuilder.DropColumn(
+                name: "PictureName",
+                table: "Menus");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "d1481d37-74aa-41d7-a687-71fbf3cc49b0", "AQAAAAIAAYagAAAAEPjyIcDsF6E2NRQ04rIaBxnjt92N8uPyda9umyzDSPG3+8a5BwCJ0+Eu8ioGoSw34w==", "dfefd378-6ba5-4c49-bcaf-25232a6ba9a3" });
+        }
+    }
+}
