e075681a31ad10598ea79b51e64bd54e16e837ac
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2024-06-11 19:05:46 +0300

    /Admin/Edit artık düzgün çalışıyor.

--- MVCHamburgerApp/Data/Entities/Menu.cs
+++ MVCHamburgerApp/Data/Entities/Menu.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVCHamburgerApp.Data.Entities
@@ -9,24 +7,15 @@ namespace MVCHamburgerApp.Data.Entities
     public class Menu
     {
         public int Id { get; set; }
-
-        [Required]
         public string Name { get; set; }
-
         public string Description { get; set; }
-
-        [Required]
-        [Range(0, 1000)]
         public decimal BasePrice { get; set; }
-
-        public string PictureName { get; set; }
+        public string? PictureName { get; set; } = "default.jpg";
 
         [NotMapped]
-        public IFormFile PictureFile { get; set; }
+        public IFormFile? PictureFile { get; set; }
 
-        [Required]
-        public string Size { get; set; } //(enum mı olacak acaba müşteriye selectbox'tan seçtirmek için)
-
+        public string Size { get; set; }
         public ICollection<OrderDetail>? OrderDetails { get; set; }
     }
 }
