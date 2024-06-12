f5f44ab563966fd7bc52091009242b21329d12dd
mehmetcagri39 <mehmetcagri.celenk@bilgeadamboost.com>
2024-06-12 19:16:20 +0300

    MenuController tarafında çalışmalar yapıldı

--- MVCHamburgerApp/Controllers/MenuController.cs
+++ MVCHamburgerApp/Controllers/MenuController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCHamburgerApp.Data;
+using MVCHamburgerApp.Models;
+using System.Threading.Tasks;
 
 namespace MVCHamburgerApp.Controllers
 {
     public class MenuController : Controller
     {
-        public IActionResult Index()
+        private readonly HamburgerDbContext _context;
+
+        public MenuController(HamburgerDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var menus = await _context.Menus.ToListAsync();
+            var extraToppings = await _context.ExtraToppings.ToListAsync();
+
+            var model = new MenuViewModel
+            {
+                Menus = menus,
+                ExtraToppings = extraToppings
+            };
+
+            return View(model);
         }
     }
 }
